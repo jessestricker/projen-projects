ad1d2ff65c064878c66ecc82aec37d7502a8f47f
Jesse Stricker <git@jessestricker.de>
2025-03-02 18:41:13 +0100

    chore: set repository url

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,18 @@
 import { cdk, javascript } from 'projen';
 const project = new cdk.JsiiProject({
+  name: 'projen-projects',
+
+  // node
+  defaultReleaseBranch: 'main',
+  packageManager: javascript.NodePackageManager.NPM,
+
+  // typescript
+  projenrcTs: true,
+
+  // jsii
+  jsiiVersion: '~5.7.0',
   author: 'Jesse Stricker',
   authorAddress: 'git@jessestricker.de',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.7.0',
-  name: 'projen-projects',
-  packageManager: javascript.NodePackageManager.NPM,
-  projenrcTs: true,
-  repositoryUrl: 'https://github.com/git/projen-projects.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  repositoryUrl: 'https://github.com/jessestricker/projen-projects.git',
 });
 project.synth();
