7713e768597f9f22b3a93955ec86af2d406a2d55
Jesse Stricker <git@jessestricker.de>
2025-03-02 18:48:57 +0100

    chore: enable prettier

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,18 +1,26 @@
-import { cdk, javascript } from 'projen';
+import { cdk, javascript } from "projen";
 const project = new cdk.JsiiProject({
-  name: 'projen-projects',
+  name: "projen-projects",
 
   // node
-  defaultReleaseBranch: 'main',
+  defaultReleaseBranch: "main",
   packageManager: javascript.NodePackageManager.NPM,
+  prettier: true,
 
   // typescript
   projenrcTs: true,
 
   // jsii
-  jsiiVersion: '~5.7.0',
-  author: 'Jesse Stricker',
-  authorAddress: 'git@jessestricker.de',
-  repositoryUrl: 'https://github.com/jessestricker/projen-projects.git',
+  jsiiVersion: "~5.7.0",
+  author: "Jesse Stricker",
+  authorAddress: "git@jessestricker.de",
+  repositoryUrl: "https://github.com/jessestricker/projen-projects.git",
 });
+
+project.prettier!.ignoreFile!.exclude(
+  ...project.files.map((file) => file.path),
+  project.package.lockFile,
+);
+project.addTask("prettier", { exec: "prettier --write ." });
+
 project.synth();
